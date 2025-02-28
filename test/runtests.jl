2d49c2fe0620b6f2897175c9de35c9c0f5dd6868
0G3NES1S <0g3nes1s@gmail.com>
2025-02-28 01:09:59 -0300

    Error `test/runtests.jl`

--- test/runtests.jl
+++ test/runtests.jl
@@ -104,37 +104,40 @@ using ArtSet
     )
 
     MyColor3 = Tools.CraftIn256(
-        BOLD = true,
-        FAINT = false,
-        ITALIC = true,
-        UNDERLINE = false,
-        BLINKING = true,
-        INVERSE = false,
-        HIDDEN = false,
-        STRIKETHROUGH = false,
-        TYPE = UInt8(38),
-        MODE = UInt8(5),
-        COLOR = UInt8(60)
+        true,
+        false,
+        true,
+        false,
+        true,
+        false,
+        false,
+        false,
+        UInt8(38),
+        UInt8(5),
+        UInt8(60)
     )
 
     MyColor4 = Tools.CraftInRGB(
-        BOLD = true,
-        FAINT = false,
-        ITALIC = true,
-        UNDERLINE = false,
-        BLINKING = true,
-        INVERSE = false,
-        HIDDEN = false,
-        STRIKETHROUGH = false,
-        TYPE = UInt8(38),
-        MODE = UInt8(5),
-        R = UInt8(10),
-        G = UInt8(20),
-        B = UInt8(30)
+        true,
+        false,
+        true,
+        false,
+        true,
+        false,
+        false,
+        false,
+        UInt8(38),
+        UInt8(5),
+        UInt8(10),
+        UInt8(20),
+        UInt8(30)
     )
 
-    @test Tools.importc(MyColor3; ID=:MyColor3Yeah!)
-    @test Tools.importc(MyColor4; ID="MyColor4Yeah!")
+    @test Tools.importc(MyColor3; ID=:MyColor3Yeah!TEST)
+    @test Tools.importc(MyColor4; ID="MyColor4Yeah!TEST")
+
+    Tools.importc(MyColor3; ID=:MyColor3Yeah!)
+    Tools.importc(MyColor4; ID="MyColor4Yeah!")
 
     @test Tools.Draw(Tools.ColorsKit[:CRAFTED][:MyColor3Yeah!]; TEXT="Hello World!")
     @test Tools.Draw(Tools.ColorsKit[:CRAFTED][:MyColor4Yeah!]; TEXT="Hello World!")
