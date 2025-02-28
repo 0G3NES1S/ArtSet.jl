f6202369f904ee4084f7962c2463d0c7a3bafc8c
0G3NES1S <0g3nes1s@gmail.com>
2025-02-28 01:36:34 -0300

    Error `test/runtests.jl"

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,39 +1,10 @@
 using Test
 using ArtSet
 
-@testset "ArtSet.jl" begin
-    @test TESTCOLOR1 = Tools.Craft256(
-        ID = :MyColorYeah!,
-        BOLD = true,
-        FAINT = false,
-        ITALIC = true,
-        UNDERLINE = false,
-        BLINKING = true,
-        INVERSE = false,
-        HIDDEN = false,
-        STRIKETHROUGH = false,
-        TYPE = UInt8(38),
-        COLOR = UInt8(255)
-    )
+@testset "Craft256 && CraftRGB" begin
 
-    @test TESTCOLOR2 = Tools.CraftRGB(
-        ID = "MyColor2Yeah!",
-        BOLD = true,
-        FAINT = false,
-        ITALIC = true,
-        UNDERLINE = false,
-        BLINKING = true,
-        INVERSE = false,
-        HIDDEN = false,
-        STRIKETHROUGH = false,
-        TYPE = UInt8(38),
-        R = UInt8(134),
-        G = UInt8(200),
-        B = UInt8(10),
-    )
-
-    MyColor = Tools.Craft256(
-        ID = :MyColorYeah!,
+    MyColor1 = Tools.Craft256(
+        ID = :MyColor1Yeah!,
         BOLD = true,
         FAINT = false,
         ITALIC = true,
@@ -62,46 +33,15 @@ using ArtSet
         B = UInt8(10),
     )
 
-    @test Tools.Draw(MyColor2; TEXT="Hello World!")
-    @test Tools.Draw(Tools.ColorsKit[:CRAFTED][:MyColor2Yeah!]; TEXT="Hello World!")
+    @test haskey(Tools.ColorsKit[:CRAFTED][:MyColor1Yeah!])
+    @test haskey(Tools.ColorsKit[:CRAFTED][:MyColor2Yeah!])
 
-    @test Tools.Draw(MyColor; TEXT="Hello World")
-    @test Tools.Draw(Tools.ColorsKit[:CRAFTED][:MyColorYeah!]; TEXT="Hello World!")
-    # -<<---->>--
+    @test typeof(MyColor1) == Tools.CraftIn256
+    @test typeof(MyColor2) == Tools.CraftInRGB 
 
-    @test Tools.Draw(:N_F_RED; TEXT="Hello World!")
+end
 
-    # -<<---->>--
-
-    @test TESTCOLOR3 = Tools.CraftIn256(
-        BOLD = true,
-        FAINT = false,
-        ITALIC = true,
-        UNDERLINE = false,
-        BLINKING = true,
-        INVERSE = false,
-        HIDDEN = false,
-        STRIKETHROUGH = false,
-        TYPE = UInt8(38),
-        MODE = UInt8(5),
-        COLOR = UInt8(60)
-    )
-
-    @test TESTCOLOR4 = Tools.CraftInRGB(
-        BOLD = true,
-        FAINT = false,
-        ITALIC = true,
-        UNDERLINE = false,
-        BLINKING = true,
-        INVERSE = false,
-        HIDDEN = false,
-        STRIKETHROUGH = false,
-        TYPE = UInt8(38),
-        MODE = UInt8(5),
-        R = UInt8(10),
-        G = UInt8(20),
-        B = UInt8(30)
-    )
+@testset "CraftIn256 && CraftInRGB" begin
 
     MyColor3 = Tools.CraftIn256(
         true,
@@ -133,13 +73,45 @@ using ArtSet
         UInt8(30)
     )
 
-    @test Tools.importc(MyColor3; ID=:MyColor3Yeah!TEST)
-    @test Tools.importc(MyColor4; ID="MyColor4Yeah!TEST")
-
     Tools.importc(MyColor3; ID=:MyColor3Yeah!)
     Tools.importc(MyColor4; ID="MyColor4Yeah!")
 
-    @test Tools.Draw(Tools.ColorsKit[:CRAFTED][:MyColor3Yeah!]; TEXT="Hello World!")
-    @test Tools.Draw(Tools.ColorsKit[:CRAFTED][:MyColor4Yeah!]; TEXT="Hello World!")
+    @test haskey(Tools.ColorsKit[:CRAFTED][:MyColor3Yeah!])
+    @test haskey(Tools.ColorsKit[:CRAFTED][:MyColor4Yeah!])
 
+end
+
+@testset "Draw" begin
+
+    MyColor1 = Tools.Craft256(
+        ID = :MyColor1Yeah!,
+        BOLD = true,
+        FAINT = false,
+        ITALIC = true,
+        UNDERLINE = false,
+        BLINKING = true,
+        INVERSE = false,
+        HIDDEN = false,
+        STRIKETHROUGH = false,
+        TYPE = UInt8(38),
+        COLOR = UInt8(255)
+    )
+
+    MyColor2 = Tools.CraftIn256(
+        true,
+        false,
+        true,
+        false,
+        true,
+        false,
+        false,
+        false,
+        UInt8(38),
+        UInt8(5),
+        UInt8(60)
+    )
+
+    Tools.Draw(MyColor1; TEXT="Hello World!")
+    Tools.Draw(MyColor2; TEXT="Hello World!")
+    
 end
